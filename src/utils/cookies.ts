e3478441a7e38ed1b4a667fd74fa3e887e6a3e24
Danylo Bilyi <danilo.bilyi@gmail.com>
2023-10-24 04:23:50 +0300

    feat: add cookies options

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,14 +1,16 @@
 import { AuthTokens } from 'api/auth/types';
-import { Response } from 'express';
+import { CookieOptions, Response } from 'express';
 
 import { ACCESS_TOKEN_COOKIE, REFRESH_TOKEN_COOKIE } from './constants';
 
+const cookiesOptions: CookieOptions = { sameSite: 'none', secure: true };
+
 export const setAuthCookies = (tokens: AuthTokens, res: Response) => {
-  res.cookie(REFRESH_TOKEN_COOKIE, tokens.refresh_token);
-  res.cookie(ACCESS_TOKEN_COOKIE, tokens.access_token);
+  res.cookie(REFRESH_TOKEN_COOKIE, tokens.refresh_token, cookiesOptions);
+  res.cookie(ACCESS_TOKEN_COOKIE, tokens.access_token, cookiesOptions);
 };
 
 export const clearAuthCookies = (res: Response) => {
-  res.clearCookie(REFRESH_TOKEN_COOKIE);
-  res.clearCookie(ACCESS_TOKEN_COOKIE);
+  res.clearCookie(REFRESH_TOKEN_COOKIE, cookiesOptions);
+  res.clearCookie(ACCESS_TOKEN_COOKIE, cookiesOptions);
 };
