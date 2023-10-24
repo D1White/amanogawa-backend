f5516eab295b229bfbcd8303c1a94cf47e9dde03
Danylo Bilyi <danilo.bilyi@gmail.com>
2023-10-24 04:45:03 +0300

    feat: add cookies maxAge

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -6,8 +6,14 @@ import { ACCESS_TOKEN_COOKIE, REFRESH_TOKEN_COOKIE } from './constants';
 const cookiesOptions: CookieOptions = { sameSite: 'none', secure: true };
 
 export const setAuthCookies = (tokens: AuthTokens, res: Response) => {
-  res.cookie(REFRESH_TOKEN_COOKIE, tokens.refresh_token, cookiesOptions);
-  res.cookie(ACCESS_TOKEN_COOKIE, tokens.access_token, cookiesOptions);
+  res.cookie(REFRESH_TOKEN_COOKIE, tokens.refresh_token, {
+    ...cookiesOptions,
+    maxAge: 1000 * 60 * 60,
+  });
+  res.cookie(ACCESS_TOKEN_COOKIE, tokens.access_token, {
+    ...cookiesOptions,
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+  });
 };
 
 export const clearAuthCookies = (res: Response) => {
